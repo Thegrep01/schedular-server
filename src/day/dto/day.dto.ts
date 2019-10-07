75a852e647550d058b6f040c5ae65f76eb8426a7
Denis <den.belotserkovets@gmail.com>
2019-10-07 22:00:41 +0300

    update

--- src/day/dto/day.dto.ts
+++ src/day/dto/day.dto.ts
@@ -2,5 +2,6 @@ import { LessonDto } from './lesson.dto';
 
 export class DayDto {
     public readonly index!: number;
+    public readonly name!: string;
     public readonly lessons?: LessonDto[];
 }
--- src/day/service/day.service.ts
+++ src/day/service/day.service.ts
@@ -1,6 +1,12 @@
 import { DayDto } from './../dto/day.dto';
 export class DaysService {
     public async getAll(): Promise<DayDto[]> {
-        return [{ index: 1 }, { index: 2 }, { index: 3 }, { index: 4 }, { index: 5 }, { index: 6 }, { index: 7 }];
+        return [
+        { index: 1, name: 'Monday' },
+        { index: 2, name: 'Tuesday' },
+        { index: 3, name: 'Wednesday' },
+        { index: 4, name: 'Thursday' },
+        { index: 5, name: 'Friday' },
+        { index: 6, name: 'Saturday' }];
     }
 }
